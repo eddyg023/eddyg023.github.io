1eab3107103cfe4fe51ef3082e0deba5640dfe35
Eddy Guo <guo.eddy023@gmail.com>
2024-06-29 17:22:59 -0400

    Changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let topButton = document.getElementById("top-button");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
       mybutton.style.display = "block";
     } else {
       mybutton.style.display = "none";
