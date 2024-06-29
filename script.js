5ba76fadf89e14275bb9662d56df72e3cd08d7cb
Eddy Guo <guo.eddy023@gmail.com>
2024-06-28 23:36:18 -0400

    Changes

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function displayEmail() {
-    const email = document.querySelector('.email');
-    email.textContent = 'guo.eddy023@gmail.com';
-  }
-
-function hideEmail() {
-    const email = document.querySelector('.email');
-    email.textContent = 'Email';
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const email = document.querySelector('.email');
-    email.addEventListener('click', displayEmail);
-    email.addEventListener('mouseout', hideEmail);
-});
-
-
 let topButton = document.getElementById("top-button");
 window.onscroll = function() {scrollFunction()};
 
@@ -31,3 +13,7 @@ function scrollFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+
+
+  
